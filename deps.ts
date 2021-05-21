f0c312eed57375cbdc223df5b223c0471dd5c28a
ely <eouldsmail@gmail.com>
2021-05-21 14:34:46 +0000

    commit 3

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-import { Application, Router,send ,Context,Middleware} from "https://deno.land/x/oak/mod.ts";
+import { Application, Router,send ,Context,Middleware} from "https://deno.land/x/oak@v5.7.0/mod.ts";
 export { Application, Router, send,Context}
 export type{Middleware }
 
-import { Query,Where } from "https://deno.land/x/sql_builder/mod.ts";
+import { Query,Where } from "https://deno.land/x/sql_builder@v1.8.0/mod.ts";
 export  { Query ,Where}
 
-import { Client } from "https://deno.land/x/mysql/mod.ts";
+import { Client } from "https://deno.land/x/mysql@v2.9.0/mod.ts";
 export { Client }
 
-import * as oakMiddleware from "https://deno.land/x/oak_middleware/mod.ts";
-export {oakMiddleware}
+// import * as oakMiddleware from "https://deno.land/x/oak_middleware/mod.ts";
+// export {oakMiddleware}
